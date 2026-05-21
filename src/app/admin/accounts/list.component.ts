989911c575ccb121b296681436d9950666b657ae
ROBLERJ-UI <rjroble2921@gmail.com>
2026-05-21 12:31:16 +0800

    Fix accounts list loading

--- src/app/admin/accounts/list.component.ts
+++ src/app/admin/accounts/list.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { AccountService } from '@app/_services';
 import { Account } from '@app/_models';
+import { filter } from 'rxjs/operators';
 
 @Component({ standalone: false, templateUrl: 'list.component.html' })
 export class ListComponent implements OnInit {
     accounts!: Account[];
 
-    constructor(private accountService: AccountService) {}
+    constructor(
+        private accountService: AccountService,
+        private router: Router
+    ) {}
 
     ngOnInit() {
-    this.accountService.getAll()
-        .subscribe({
-            next: accounts => {
-                console.log('Accounts loaded:', accounts);
-                this.accounts = accounts;
-            },
-            error: error => {
-                console.error('Failed to load accounts:', error);
-                this.accounts = [];
-            }
-        });
-}
+        this.loadAccounts();
+    }
+
+    loadAccounts() {
+        this.accountService.getAll()
+            .subscribe({
+                next: accounts => {
+                    console.log('Accounts loaded:', accounts);
+                    this.accounts = accounts;
+                },
+                error: error => {
+                    console.error('Failed to load accounts:', error);
+                    this.accounts = [];
+                }
+            });
+    }
 
     deleteAccount(account: any) {
         account.isDeleting = true;
